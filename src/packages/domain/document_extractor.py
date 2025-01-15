3634f7d181fe7c5a25ceef5484f5773d45683d1b
takata-kento <takata_kento@ims-sol.co.jp>
2025-01-15 10:57:31 +0900

    refactor: fix interface method of document extractor

--- src/packages/domain/document_extractor.py
+++ src/packages/domain/document_extractor.py
@@ -1,9 +1,26 @@
-from abc import ABCMeta, abstractmethod
-from typing import List, Optional, IO
+"""Extracting documents.
 
-from azure.ai.documentintelligence.models import AnalyzeResult
+This module provides class to extract various document.
+"""
+from abc import ABCMeta, abstractmethod
+from typing import IO
 
 class DocumentExtractor(metaclass=ABCMeta):
+    """Extract document"""
+
     @abstractmethod
-    def extract(self, document: IO) -> AnalyzeResult:
+    def extract(
+        self,
+        document: IO
+    ) -> dict[str, any]:
+        """Extract document by using Cloud Service.
+
+        Args:
+            document:
+                Document to be extracted.
+
+        Returns:
+            JSON response of extracted document.
+        """
+
         raise NotImplementedError()
